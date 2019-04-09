40c7bde729f95d5d05854fdeb0d4d35baacc7a1c
De Chapuiset Le Merle <e1600752@edu.vamk.fi>
2019-04-09 09:01:40 +0300

    first commit

---
+++ src/test/java/fi/vamk/tka/reactspring/SpringTest.java
@@ -0,0 +1,13 @@
+package fi.vamk.tka.reactspring;
+import org.junit.Test;
+public class SpringTest{
+    @Test
+    public void addingGroupTest(){
+        System.out.println("Onko kevät koittanut");
+    }
+    
+    @Test
+    public void helloYouTest(){
+        System.out.println("Onko kesä koittanut");
+    }
+}
